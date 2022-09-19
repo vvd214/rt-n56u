b9cc2db0d3c99a1b9e4d68f2326c9e973b13e98f
vvd214 <vvd2104@gmail.com>
2022-09-19 21:38:42 +0700

    fix timezone to UTC-7

--- trunk/user/shared/defaults.h
+++ trunk/user/shared/defaults.h
@@ -31,8 +31,8 @@
 #define SYS_GROUP_NOGROUP	"nogroup"
 
 #define DEF_LAN_ADDR		"192.168.2.1"
-#define DEF_LAN_DHCP_BEG	"192.168.2.100"
-#define DEF_LAN_DHCP_END	"192.168.2.244"
+#define DEF_LAN_DHCP_BEG	"192.168.2.10"
+#define DEF_LAN_DHCP_END	"192.168.2.253"
 #define DEF_LAN_MASK		"255.255.255.0"
 
 #define DEF_WLAN_2G_CC		"CN"
@@ -46,9 +46,9 @@
 
 #define DEF_ROOT_PASSWORD	"admin"
 #define DEF_SMB_WORKGROUP	"WORKGROUP"
-#define DEF_TIMEZONE		"CST-8"
-#define DEF_NTP_SERVER0		"ntp1.aliyun.com"
-#define DEF_NTP_SERVER1		"2001:470:0:50::2"
+#define DEF_TIMEZONE		"UTC-7"
+#define DEF_NTP_SERVER0		"0.pool.ntp.org"
+#define DEF_NTP_SERVER1		"1.pool.ntp.org"
 #ifdef SUPPORT_OPENSSL_EC
 #define DEF_HTTPS_CIPH_LIST	"ECDH+CHACHA20:ECDH+AESGCM:DH+AESGCM:DH+AES256:DH+AES:DH+3DES:RSA+AES:RSA+3DES:!ADH:!MD5:!DSS"
 #else
